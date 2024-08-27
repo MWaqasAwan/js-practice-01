b0bea7179f870018cd8877c10e0df6c1742bf395
Muhammad Waqas Awan <106952032+MWaqasAwan@users.noreply.github.com>
2024-08-27 14:34:29 +0000

    heat and stack

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,34 @@
+
+//let score = [1,2,3] /// return true
+// let score = {}  // return true
+//let score = "" // empty string return false
+//let score = "waqas" // in string put the value then return true
+
+// let score = "abc" // return value nan conversion in number
+// let score = "" // empty string convert in number return value  0 
+// let score = [1,2,3]
+// let boolconv = Boolean(score)
+// let number = Number(score)
+
+// console.log(number)
+// console.log(typeof number)
+
+// let name = "Waqas";
+// let cname = name;
+// cname = "Waqas Awan"
+// console.log(name);
+// console.log(cname);
+
+// let userone = {
+//     email : "waqas@gmail.com",
+//     jazzcash : 22
+// }
+
+// let usertwo = userone
+// usertwo.email = "awan@gmail.com"
+// console.log(usertwo);
+// console.log(userone);
+
+
+
+
